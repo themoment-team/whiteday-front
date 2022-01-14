1f47b64b460a4ba429031f8f73442f125709a33e
hyeongrok7874 <khr5k0228@gmail.com>
2022-01-14 12:27:32 +0900

    Add Background2

---
+++ src/Components/Backgrounds/Background2/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const Background2: React.FC = () => {
+  return <S.Background />
+}
+
+export default Background2;
---
+++ src/Components/Backgrounds/Background2/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #ffecdb;
+  box-shadow: 4px 4px 100px 50px rgba(253, 87, 147, 0.25);
+`;
