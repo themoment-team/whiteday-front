5d750f03ae2fc329f71685194f3d72780a097f5a
yoosion030 <rsy5487@naver.com>
2022-01-15 16:25:32 +0900

    Add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./Styles/GlobalStyle";
 import * as P from "./Pages";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/"></Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </>
   );
 };
 
