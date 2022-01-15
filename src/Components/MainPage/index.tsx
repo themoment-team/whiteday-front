6013cf87b15b1352f7d3bd020ce4928a333f4c65
hyeongrok7874 <khr5k0228@gmail.com>
2022-01-15 14:50:59 +0900

    Add MainPage component

---
+++ src/Components/MainPage/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Background from "../Background";
+
+const MainPage: React.FC = () => {
+  return <Background brownBoxVisible={true}/>
+};
+
+export default MainPage;
