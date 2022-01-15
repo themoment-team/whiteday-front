e97f4873a4bf0a5dcefbccf39c2b6f675a5f2303
yoosion030 <rsy5487@naver.com>
2022-01-15 16:07:36 +0900

    Delete BrowserRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import * as P from "./Pages";
 
 const App: React.FC = () => {
