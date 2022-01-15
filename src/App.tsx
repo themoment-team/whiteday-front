09fa186da3bdc2a22f738420bc389d3338bab962
변찬우 <chan00106@naver.com>
2022-01-15 22:36:13 +0900

    Update index.css
    - GlobalStyle을 지우고 index.css에 옮겼습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import GlobalStyle from "./Styles/GlobalStyle";
 import * as P from "./Pages";
 
 const App: React.FC = () => {
   return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/"></Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" />
+    </Routes>
   );
 };
 
