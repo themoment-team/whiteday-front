635940a7f433dcb15288ad8f4d3bc6de673a227a
hyeongrok7874 <khr5k0228@gmail.com>
2022-01-16 16:38:37 +0900

    Add page explanation & candymachine

--- src/Components/MainPage/index.tsx
+++ src/Components/MainPage/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Background from "../Background";
+import * as S from "./style"
 
 const MainPage: React.FC = () => {
-  return <Background brownBoxVisible={true}/>
+  const user: string = "김형록";
+  const candyAmount: number = 999;
+  return (
+    <S.MainPage>
+      <Background brownBoxVisible={true} />
+      <S.Explanation>
+        {user}님<S.Pink>의</S.Pink> 캔디머신
+      </S.Explanation>
+      <S.Explanation>
+        {candyAmount}개<S.Pink>의</S.Pink> 사탕<S.Pink>이 들어있어요</S.Pink>
+      </S.Explanation>
+      <S.CandyMachine src="/Images/CandyMachine.png" alt="candymachine" />
+    </S.MainPage>
+  );
 };
 
 export default MainPage;
---
+++ src/Components/MainPage/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import device from "../../Shared/Config";
+
+export const MainPage = styled.div`
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  @media ${device.mobile} {
+    width: 100vw;
+    /* display: flex; */
+    /* align-items: center; */
+  }
+`;
+
+export const Explanation = styled.p`
+  position: relative;
+  top: 10vh;
+  text-align: center;
+  z-index: 1;
+  margin: 0;
+  font-size: 30px;
+  font-weight: bold;
+  color: #885252;
+`;
+
+export const Pink = styled.span`
+  color: #ff94bb;
+`;
+
+export const CandyMachine = styled.img`
+  width: 100%;
+  position: relative;
+  top: 16vh;
+  z-index: 1;
+  @media ${device.mobile} {
+    top: 17vh;
+  }
+`;
