da7a854de1cabdc533c66fb95a8b9a83a6970389
Hyeongrok <khr5k0228@gmail.com>
2022-01-26 03:38:29 +0900

    Update style

--- src/Components/Background/style.ts
+++ src/Components/Background/style.ts
@@ -7,7 +7,7 @@ interface maxHeightType{
 
 export const Background = styled.div<maxHeightType>`
   width: 600px;
-  height: 100%;
+  height: 100vh;
   box-shadow: 4px 4px 100px 50px rgba(253, 87, 147, 0.25);
   position: absolute;
   @media ${device.mobile} {
