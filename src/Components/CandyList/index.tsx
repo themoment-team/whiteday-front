a1d325187df67325aaaa7312e8026945aec65ff1
hyeongrok7874 <khr5k0228@gmail.com>
2022-02-27 14:16:06 +0900

    Update codes

--- src/Components/CandyList/index.tsx
+++ src/Components/CandyList/index.tsx
@@ -10,13 +10,12 @@ const CandyList: React.FC = () => {
   const logged: boolean = true;
   const [candyBundle, setCandyBundle] = useState<object[]>([]);
 
-  let processedCandies:object[] = [];
-
   useEffect(() => {
     axios.get("/v1/member/39fa000e25fc0ba48c6b9488d16e016cd052d32dee483fb9d3592d51a37b1918")
       .then(response => {
         const candies: object[] = response.data.data.candies;
         const count = Math.ceil(candies.length / 7);
+        let processedCandies: object[] = [];
         for (let i: number = 0; i < count; i++)
           processedCandies.push({ ...candies.slice(i * 7, (i + 1) * 7), select: i === 0 ? true : false, id: i });
         setCandyBundle([...processedCandies]);
