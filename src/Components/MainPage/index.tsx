5e072589e5aa0c8c113abe4c66c440ef77e9911f
yoosion030 <rsy5487@naver.com>
2022-02-27 22:54:54 +0900

    Add MessageModal

--- src/Components/MainPage/index.tsx
+++ src/Components/MainPage/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import * as S from "./style"
+import React, { useState } from "react";
+import Message from "../Message";
+import * as S from "./style";
+import { isShowMessage } from "../../Atoms/AtomContainer";
+import { useRecoilState } from "recoil";
 
 const MainPage: React.FC = () => {
   const user: string = "김형록";
   const candyAmount: number = 999;
+  const [show, setShow] = useRecoilState(isShowMessage);
   return (
     <S.MainPage>
       <S.Explanation>
@@ -14,7 +18,12 @@ const MainPage: React.FC = () => {
           {candyAmount}개<S.Pink>의</S.Pink> 사탕<S.Pink>이 들어있어요</S.Pink>
         </S.CountExplanation>
       </S.Explanation>
-      <S.CandyMachine src="/Images/CandyMachine.png" alt="candymachine" />
+      <S.CandyMachine
+        src="/Images/CandyMachine.png"
+        alt="candymachine"
+        onClick={() => setShow(true)}
+      />
+      {show ? <Message /> : null}
     </S.MainPage>
   );
 };
