c6857875abea782bb92fe784de795b7144f3d0e3
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-02 19:08:40 +0900

    Update candy list

--- src/Components/CandyList/index.tsx
+++ src/Components/CandyList/index.tsx
@@ -1,6 +1,8 @@
 import * as S from "./style"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useRecoilState } from "recoil";
+import { machineIndexAtom } from "../../Atoms"
 
 interface bundleType {
   select: boolean
@@ -9,20 +11,23 @@ interface bundleType {
 const CandyList: React.FC = () => {
   const logged: boolean = true;
   const [candyBundle, setCandyBundle] = useState<object[]>([]);
+  const [machineIndex, setMachineIndex] = useRecoilState(machineIndexAtom);
 
   useEffect(() => {
-    axios.get("/v1/member/39fa000e25fc0ba48c6b9488d16e016cd052d32dee483fb9d3592d51a37b1918")
+    axios.get("/v1/login/info")
       .then(response => {
-        const candies: object[] = response.data.data.candies;
-        const count = Math.ceil(candies.length / 7);
+        const count = Math.ceil(response.data.data.candies.length / 7);
         let processedCandies: object[] = [];
         for (let i: number = 0; i < count; i++)
-          processedCandies.push({ ...candies.slice(i * 7, (i + 1) * 7), select: i === 0 ? true : false, id: i });
+          processedCandies.push({select: i === 0 ? true : false, id: i})
         setCandyBundle([...processedCandies]);
       })
   }, [])
 
   const select = (id: number) => {
+    console.log(candyBundle)
+    setMachineIndex(id);
+    console.log(machineIndex)
     setCandyBundle(
       candyBundle.map((bundle: object, index: number) => 
         index === id ? { ...bundle, select: true } : { ...bundle, select: false }
