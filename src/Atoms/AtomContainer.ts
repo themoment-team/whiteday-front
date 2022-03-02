bcf3dea8cb46ed277e86bb7135ae098d62ace650
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-02 18:08:15 +0900

    Add machineIndexAtom

---
+++ src/Atoms/AtomContainer.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil"
+
+export const machineIndexAtom = atom({
+  key: 'machineIndex',
+  default: 0,
+})
--- src/Atoms/index.ts
+++ src/Atoms/index.ts
@@ -1,1 +1,1 @@
-export {}
+export { machineIndexAtom } from './AtomContainer';
