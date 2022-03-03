c87632cc1a2720b1ea992a2e3d024b1b7cc49513
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-03 20:23:22 +0900

    Update candy list

--- src/Components/CandyList/index.tsx
+++ src/Components/CandyList/index.tsx
@@ -1,15 +1,15 @@
 import * as S from "./style"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useRecoilState } from "recoil";
-import { machineIndexAtom } from "../../Atoms"
+import { useRecoilState, useRecoilValue } from "recoil";
+import { machineIndexAtom, loggedAtom } from "../../Atoms"
 
 interface bundleType {
   select: boolean
 }
 
 const CandyList: React.FC = () => {
-  const logged: boolean = true;
+  const logged = useRecoilValue(loggedAtom)
   const [candyBundle, setCandyBundle] = useState<object[]>([]);
   const [machineIndex, setMachineIndex] = useRecoilState(machineIndexAtom);
 
@@ -25,9 +25,7 @@ const CandyList: React.FC = () => {
   }, [])
 
   const select = (id: number) => {
-    console.log(candyBundle)
     setMachineIndex(id);
-    console.log(machineIndex)
     setCandyBundle(
       candyBundle.map((bundle: object, index: number) => 
         index === id ? { ...bundle, select: true } : { ...bundle, select: false }
@@ -37,7 +35,7 @@ const CandyList: React.FC = () => {
 
   return (
     <S.CandyList>
-      {logged && candyBundle.map((bundle:object, index: number) => (
+      {candyBundle.map((bundle: object, index: number) => (
         <S.CandyListIndex key={index} select={(bundle as bundleType).select} onClick={() => select(index)} />
       ))}
     </S.CandyList>
