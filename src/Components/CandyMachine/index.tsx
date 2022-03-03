e8d30c560e899cc6b06c93f4a98e347122fb8ca1
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-03 20:22:49 +0900

    Add candymachine

--- src/Components/CandyMachine/index.tsx
+++ src/Components/CandyMachine/index.tsx
@@ -1,34 +1,43 @@
 import React, {useEffect, useState} from "react";
 import * as S from "./style"
 import axios from "axios";
-import { useRecoilValue } from "recoil"
-import { machineIndexAtom } from "../../Atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { machineIndexAtom, isShowMessage, candyIndexAtom } from "../../Atoms";
 
 interface candyType{
   color: string
+  writer: string
+  shape: string
 }
 
 
 const CandyMachine: React.FC = () => {
-  let name: string;
-  const [candies, setCandies] = useState<object[]>([])
+  const [candies, setCandies] = useState<object[]>([]);
   const machineIndex = useRecoilValue(machineIndexAtom)
+  const setShow = useSetRecoilState(isShowMessage)
+  const setCandyIndex = useSetRecoilState(candyIndexAtom)
 
   useEffect(() => {
     axios.get("/v1/login/info")
       .then(response => {
         setCandies(response.data.data.candies.slice(machineIndex * 7, (1 + machineIndex) * 7));
         console.log(candies)
-        // name = candies[0].color;
-        // console.log(name)
       })
   }, [machineIndex])
+
+  const showCandy = (index: number) => {
+    setCandyIndex(index)
+    new Date() < new Date("2022-3-14")
+      ? alert("사탕은 3월 14일에 확인 가능해요!")
+      : setShow(true);
+  }
   
   return (
     <S.CandyMachine>
+      <S.CandyMachineImg src="/Images/CandyMachine.png"/>
       <S.CandyBox>
         {candies.map((candy: object, index: number) => (
-          <S.Candy key={index} color={`#${(candy as candyType).color}`} index={index}></S.Candy>
+          <S.Candy key={index} shape={(candy as candyType).shape} color={`#${(candy as candyType).color}`} index={index} onClick={() => showCandy(index)}>{(candy as candyType).writer}</S.Candy>
         ))}
       </S.CandyBox>
     </S.CandyMachine>
--- src/Components/CandyMachine/style.ts
+++ src/Components/CandyMachine/style.ts
@@ -1,35 +1,63 @@
 import styled from "styled-components";
+import device from "../../Shared/Config"
 
 interface candyType {
-  color: string,
+  color: string
   index: number
+  shape: string
 }
 
 export const CandyMachine = styled.div`
   width: 100%;
-  height: 600px;
-  background-image: url("/Images/CandyMachine.png");
-  background-size: contain;
-  background-repeat: no-repeat;
   display: flex;
   justify-content: center;
 `
 
+export const CandyMachineImg = styled.img`
+  width: 100%;
+`
+
 export const CandyBox = styled.div`
-  width: 220px;
-  height: 220px;
-  position: relative;
-  top: 100px;
+  width: 37%;
+  height: 22.5%;
+  position: absolute;
+  top: 30%;
+  @media (min-width: 600px){
+    width: 220px;
+    height: 240px;
+  }
 `
 
 export const Candy = styled.div<candyType>`
-  width: 20%;
-  height: 20%;
+  width: 21%;
+  height: 12%;
+  font-size: 8px;
   border-radius: 100%;
+  text-align: center;
   background-color: ${({ color }) => color};
+  background-image: ${({shape}) => shape !== "none" && `url("/Images/candyShape/${shape}.png")`};
+  background-size: contain;
+  background-repeat: no-repeat;
   position: relative;
-  top: 0;
-  left: ${({ index }) => index === (2 || 5) && "0"};
-  right: ${({ index }) => index === (3 || 6) && "0"};
-  ${({ index }) => index === (1 || 4 || 7) && "margin: 0 auto"}
+  display: inline-block;
+  padding-top: 7%;
+  color: #885252;
+  ${({ index }) => {
+    switch (index) {
+      case 0:
+        return "top: 0; left: 42%;";
+      case 1:
+        return "top: 17%; left: -21%";
+      case 2:
+        return "top: 17%; left: 43%";
+      case 3:
+        return "top: 37%; left: -21%";
+      case 4:
+        return "top: 35%; left: 0%";
+      case 5:
+        return "top: 35%; left: 64%";
+      case 6:
+        return "top: 55%;";
+    }
+  }}
 `;
