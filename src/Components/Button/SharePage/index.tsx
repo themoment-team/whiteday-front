3aac14fdfb8e2ff5c9c25a6bdfa025069685d4a6
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-09 14:08:43 +0900

    Update sharepage button link

--- src/Components/Button/SharePage/index.tsx
+++ src/Components/Button/SharePage/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import * as S from "./style"
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const SharePageButton: React.FC = () => {
+  const {member_uri} = useParams();
+
   return (
     <S.Button>
-        <Link to="/leaveCandy">
+        <Link to={"/" + member_uri + "/leaveCandy"}>
           <S.Desc>메세지 남기러 가기</S.Desc>
         </Link>
     </S.Button>
