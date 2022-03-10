53c1137a92735aa221e631a909ed8b925c2e79e5
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-10 20:23:37 +0900

    Update api

---
+++ src/Shared/Config.ts
@@ -0,0 +1,12 @@
+const size = {
+  mobile: "600px",
+};
+
+const device = {
+  mobile: `(max-width: ${size.mobile})`,
+};
+
+export const baseUrl =
+  "http://server.givecandy.me:8080";
+
+export default device;
