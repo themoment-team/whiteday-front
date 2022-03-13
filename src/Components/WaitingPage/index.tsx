e0b860ac8847f4afeed512cc2b504d86c241a23f
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-13 16:18:04 +0900

    Add WaitingPage

---
+++ src/Components/WaitingPage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+
+const WaitingPage: React.FC = () => {
+  return (
+    <S.WaitingPage>
+      <S.Content>
+        <S.Text>잠시만요...</S.Text>
+      </S.Content>
+    </S.WaitingPage>
+  );
+}
+
+export default WaitingPage
---
+++ src/Components/WaitingPage/style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import device from "../../Shared/Config";
+
+export const WaitingPage = styled.div`
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #ffecdb;
+  box-shadow: 4px 4px 100px 50px rgba(253, 87, 147, 0.25);
+  display: flex;
+  align-items: center;
+  @media ${device.mobile} {
+    width: 100vw;
+  }
+`;
+
+export const Content = styled.div`
+  border-top: 5px dashed #885252;
+  border-bottom: 5px dashed #885252;
+  width: 100%;
+  height: 92%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.div`
+  font-size: 3vh;
+  color: #FD5793;
+`;
