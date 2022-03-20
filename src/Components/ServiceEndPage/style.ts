0475ac9b112970907fab5197666f7662e994f4fb
hyeongrok7874 <khr5k0228@gmail.com>
2022-03-21 00:27:45 +0900

    Add style

--- src/Components/ServiceEndPage/style.ts
+++ src/Components/ServiceEndPage/style.ts
@@ -8,6 +8,8 @@ export const ServiceEndPage = styled.div`
   width: 600px;
   height: 100vh;
   margin: 0 auto;
+  background-color: #ffecdb;
+  box-shadow: 4px 4px 100px 50px rgba(253, 87, 147, 0.25);
   @media ${device.mobile} {
     width: 100vw;
   }
